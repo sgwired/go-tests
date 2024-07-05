675a3e573bcc94f05134b6564aefb51bdbb49df2
Shelton Gayles <sg.bythelake@gmail.com>
2024-07-05 09:22:03 -0700

    add sumall func

--- arrays/sum.go
+++ arrays/sum.go
@@ -11,6 +11,16 @@ func Sum(numbers []int) int {
 	return sum
 }
 
+func SumAll(numbersToSum ...[]int) []int {
+
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
 // func main() {
 // 	var numbers = [5]int{1, 2, 3, 4, 5}
 // 	Sum(numbers)
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -14,4 +17,17 @@ func TestSum(t *testing.T) {
 			t.Errorf("go %d want %d given %d", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	// if got != want {
+	// 	t.Errorf("got %v want %v", got, want)
+	// }
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+
 }
